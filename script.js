3153300e179d5739acd259ba56071787b44cde7a
shreyasriv12 <146077117+shreyasriv12@users.noreply.github.com>
2026-07-11 00:35:37 +0530

    Implement theme toggle for dark and light modes
    Added functionality to toggle between light and dark themes, including saving the theme preference in local storage.

--- script.js
+++ script.js
@@ -116,3 +116,24 @@ function searchNotes() {
 
     });
 }
+// Dark Mode
+function toggleTheme(){
+
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+        localStorage.setItem("theme","dark");
+        document.getElementById("themeBtn").innerHTML="☀️ Light Mode";
+    }
+    else{
+        localStorage.setItem("theme","light");
+        document.getElementById("themeBtn").innerHTML="🌙 Dark Mode";
+    }
+
+}
+
+// Load Theme
+if(localStorage.getItem("theme")==="dark"){
+    document.body.classList.add("dark");
+    document.getElementById("themeBtn").innerHTML="☀️ Light Mode";
+}
